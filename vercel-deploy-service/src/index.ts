62d0d57112958fd571afe295e50b43b9badaeba0
ChethanShetty563 <chethan563@gmail.com>
2024-10-23 23:43:51 +0530

    create express server

---
+++ vercel-deploy-service/src/index.ts
@@ -0,0 +1,13 @@
+// const express = require('express');
+// const cors = require('cors');
+
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+
+
+app.listen(3000, () => {
+    console.log("Server started!")
+})
