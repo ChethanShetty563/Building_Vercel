237551c94fb3ca6eea94332375a27c99fae3ebaa
Chethan B M <35001882+ChethanShetty563@users.noreply.github.com>
2024-10-29 21:27:23 +0530

    Update aws.ts

--- vercel-upload-service/src/aws.ts
+++ vercel-upload-service/src/aws.ts
@@ -2,10 +2,10 @@ import fs from "fs";
 import AWS, { S3 } from "aws-sdk";
 
 const s3 = new AWS.S3({
-  accessKeyId: "2b6603d3001c50bc74e8397ed6932824",
+  accessKeyId: "",
   secretAccessKey:
-    "c7b5a9d68b3d1dbbda7a52584e67b253f66abca0638c54a00135690f16865bba",
-  endpoint: "https://58ffbe702fdceddb387f04368b61dfd2.r2.cloudflarestorage.com",
+    "",
+  endpoint: "",
   s3ForcePathStyle: true,
   signatureVersion: "v4",
   region: "auto",
